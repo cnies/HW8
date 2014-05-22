061408f37b0a8de83848ead58177382fedcf7820
Christopher <cnies@ucsd.edu>
2014-05-22 16:53:19 -0700

    More stubs

--- Quantity.java
+++ Quantity.java
@@ -18,6 +18,25 @@ public class Quantity{
 	private Map<String, Integer> theUnits; /*The Units with their exponents*/
 
 	////////////////////CONSTRUCTORS/////////////////////
+	/*
+	Constructor containing values for the quantity, and the units in the
+	numerator or denominator
+	*/
+	public Quantity(double value, List<String> unitsNum, List<String> unitsDen){
+	}
+	
+	/*
+	Deep-Copy constructor of another Quantity
+	*/
+	public Quantity(Quantity toCopy){
+	}
+
+	/*
+	Default no-arg constructor. Defaults to a value of 1.0 and
+	no units
+	*/
+	public Quantity(){
+	}
 
 
 
@@ -25,7 +44,7 @@ public class Quantity{
 
 	///////////////////PUBLIC METHODS////////////////////
 
-	//Provided for us
+	//Provided for us. Good thing too!
 	public String toString() {
 		double valueOfTheQuantity = this.quantity;
 		Map<String,Integer> mapOfTheQuantity = this.theUnits;
@@ -45,9 +64,72 @@ public class Quantity{
 			+ unitsString.toString();
 	}
 
+	//Returns true if this Quantity is the same as another. Use Java's
+	//"instanceof" to make sure the object is a Quantity
+	public boolean equals(Object toCompare){
+	}
+	
+	//Returns a hashcode for this Quantity. Pretty easy, since all we
+	//need to do is get the hash code for the String
+	public int hashCode(){
+		return this.toString().hashCode();
+	}
+
+	//All of these are operations on the Quantities, all of them throw
+	//IllegalArgumentExceptions if the arguments are null. Add and Subtract
+	//also throw this exception if they are not of the same units
+	/*
+	Used for the multiplication of this by another Quantity
+	*/
+	public Quantity mul(Quantity toMult){
+	}
+
+  /*
+	Divides this by another Quantity
+	*/
+	public Quantity div(Quantity toDiv){
+	}
+
+	/*
+	Raise this Quantity to an integer power
+	*/
+	public Quantity pow(int exponent){
+	}
+
+	/*
+	Add another Quantity of the same units to this
+	*/
+	public Quantity add(Quantity toAdd){
+	}
+	
+	/*
+	Find the difference between this and a Quantity of the same units
+  */
+	public Quantity sub(Quantity toSubtract){
+	}
+
+	/*
+	Find and return the negation of this Quantity
+	*/
+	public Quantity negate(){
+	}
+	
+	//Uses a database to return a Quantity that represents one of the input unit
+	//IE One Hour --> 3600 Seconds, or km ---> 1000m
+	public static Quantity normalizeUnit(String unit, Map<String, Quantity> database){
+	}
+
+	//Uses a database to return a copy of this, with the units converted to the
+	//primitive units as indicated by the database
+	public static Quantity normalize(Map<String, Quantity>){
+	}
 
 
 
 
 	///////////////////HELPER METHODS////////////////////
+	//Simple method to shift the exponents in for the specified by delta.
+	private void adjustExponentsBy(int delta, String unit){
+	}
+
 }
